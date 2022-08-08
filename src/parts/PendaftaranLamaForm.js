1d8592015a8d511718c5114e4e2b958c544317c2
bebbyilham <bebbyilham@gmail.com>
2022-08-08 11:58:30 +0700

    080822 1058 form pendf online

--- src/parts/PendaftaranLamaForm.js
+++ src/parts/PendaftaranLamaForm.js
@@ -569,7 +569,6 @@ function PendaftaranLamaForm({ history }) {
             </>
           )) ||
             cekmr === "error"}
-
           <div className="hidden sm:block" aria-hidden="true">
             <div className="py-5">
               <div className="border-t border-gray-200" />
